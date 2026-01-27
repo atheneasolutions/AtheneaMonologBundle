4622e8cfc2e88fd33241204f90538315183b1e10
marmelichathenea <mar.melich@atheneasolutions.com>
2026-01-27 11:30:13 +0100

    feat: fix symfony dependency injection changes

--- src/DependencyInjection/AtheneaMonologExtension.php
+++ src/DependencyInjection/AtheneaMonologExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class AtheneaMonologExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
